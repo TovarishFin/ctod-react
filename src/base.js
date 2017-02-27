5a06e150ccb17d47823b5e8c15b3fbf7b19763c5
TovarishFin <TovarishFin@yandex.ru>
2017-02-27 04:47:04 +0100

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCHUlHZQpytNQ_YFinll9s3TLiV3vPSILk",
+    authDomain: "catch-of-the-day-231f3.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-231f3.firebaseio.com"
+})
+
+export default base;
